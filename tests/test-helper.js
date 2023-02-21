c0f42f8d74bc739f018ef6673fafefed5065c54f
Dan Bjorge <dan@dbjorge.net>
2023-02-20 20:21:32 -0500

    Clean up some unnecessary references to unused ember-qunit

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -6,9 +6,6 @@ import { setup } from 'qunit-dom';
 import chai from 'chai';
 import sinonChai from 'sinon-chai';
 import {mocha} from 'mocha';
-
-// TODO: ember-qunit's start was suggested by ember-cli-update 3.28.2, need both?
-// import { start } from 'ember-qunit';
 import start from 'ember-exam/test-support/start';
 
 chai.use(sinonChai);
